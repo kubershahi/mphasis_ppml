554b5b2c370374f72833c0c6c4bd0661ead5b614
actuallysoham <soham.de2001@gmail.com>
2021-06-12 02:56:03 +0530

    Added Vectorization and Mini-batches in SGD

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ Goals:
 - Multiplying shares using triplets [done]
 - Online Phase of Linear Regression [done]
 - Ideal Functionality (Linear Regression) [done]
-- Batchwise SGD []
+- Batchwise SGD [done]
 - Use NetIO for socket communication []
 - Truncation (later)
 ====================================
@@ -20,19 +20,29 @@ Parameters:
 - n: number of samples
 - d: number of features
 - B: batch size
-- t: n/B
+- E: number of epochs
+- t: number of iterations = n.E/B
 ====================================
 Dimensions:
 - X: (n,d)
 - Y: (n,1)
 - U: (n,d)
 - V: (d,t)
-- V': (B,t)
+- VV: (B,t)
 - Z: (B,t)
-- Z': ()
+- ZZ: (d,t)
 ====================================
 */
- 
+
+// ====================================
+// Global Declarations: 
+// ==================================== 
+int N = 6; //6
+int d = 5; //5
+int B = 3; //3
+int NUM_EPOCHS = 1; // change; shuffle order
+// ====================================
+
 void share(MatrixXi A, MatrixXi shares[])
 {
 	MatrixXi A_0 = MatrixXi::Random(A.rows(),A.cols()) / 10000000;
@@ -83,101 +93,155 @@ MatrixXi linearRegression(MatrixXi X, MatrixXi Y, MatrixXi w)
 
   MatrixXi E_0 = X_0 - U_0;
   MatrixXi E_1 = X_1 - U_1;
-  MatrixXi E = rec(E_0, E_1);
+  MatrixXi E = rec(E_0, E_1); // masked X_i
 
-  cout << "Here is the matrix U + E:\n" << U + E <<endl; // sanity check, must = X
+  //cout << "Here is the matrix U + E:\n" << U + E <<endl; // sanity check, must = X
+
   // =================================================================================
-  // following to be in a for loop, for 't' epochs:
 
-  MatrixXi V = MatrixXi::Random(w.rows(),w.cols()) / 10000000; // masks Y_i
+  int t = (N * NUM_EPOCHS)/B; // E = 1
+
+  MatrixXi V = MatrixXi::Random(d, t) / 10000000; // masks w_i
   share(V, shares);
   MatrixXi V_0 = shares[0]; 
   MatrixXi V_1 = shares[1];
 
-  MatrixXi F_0 = w_0 - V_0;
-  MatrixXi F_1 = w_1 - V_1;
-  MatrixXi F = rec(F_0, F_1);
-
-  //cout << "Dim of U: " << U.rows()<<" "<<U.cols() <<endl;
-  //cout << "Dim of V: " << V.rows()<<" "<<V.cols() <<endl;
-
-  MatrixXi Z = U * V; // third triplet for multiplication
+  //MatrixXi Z = U * V; // third triplet for multiplication -> change for batchwise SGD
+  MatrixXi Z = MatrixXi::Zero(B,t); 
+  for (int z = 0; z < Z.cols(); z++)
+  {
+    Z.col(z) = U.block(z * B,0,B,U.cols()) * V.col(z);
+  }
   share(Z, shares);
   MatrixXi Z_0 = shares[0];
   MatrixXi Z_1 = shares[1];
-  // =========================== 
 
-  // YY = X.w
-  MatrixXi YY_0 = mult(0, X_0, w_0, E, F, Z_0);
-  MatrixXi YY_1 = mult(1, X_1, w_1, E, F, Z_1);
-
-  // D = X.w - Y
-  MatrixXi D_0 = YY_0 - Y_0;
-  MatrixXi D_1 = YY_1 - Y_1;
-
-  // =========================== 
-  MatrixXi VV = MatrixXi::Random(D_0.rows(),D_0.cols()) / 10000000; // masks D_i
+  MatrixXi VV = MatrixXi::Random(B,t) / 10000000; // masks D_i
   share(VV, shares);
   MatrixXi VV_0 = shares[0]; 
   MatrixXi VV_1 = shares[1];
 
-  MatrixXi FF_0 = D_0 - VV_0;
-  MatrixXi FF_1 = D_1 - VV_1;
-  MatrixXi FF = rec(FF_0, FF_1);
-
-  MatrixXi ZZ = U.transpose() * VV; // third triplet for multiplication
+  //MatrixXi ZZ = U.transpose() * VV; // third triplet for multiplication -> change for batchwise SGD
+  MatrixXi ZZ = MatrixXi::Zero(d,t);
+  for (int z = 0; z < ZZ.cols(); z++)
+  {
+    ZZ.col(z) = U.transpose().block(0,z * B,U.cols(),B) * VV.col(z);
+  }
   share(ZZ, shares);
   MatrixXi ZZ_0 = shares[0];
   MatrixXi ZZ_1 = shares[1];
-  // =========================== 
+// =========================== 
+  
+  for(int j = 0; j < t; j ++)
+  {
+    // In each iteration:
+    /* 
+    - Use E_B_j instead of E [done]
+    - Use V[j] instead of V [done]
+    - Use VV[j] instead of VV [done]
+    - Recalculate Z, ZZ [done]
 
-  // delta = X^T(X.w - Y)
-  MatrixXi delta_0 = mult(0, X_0.transpose(), D_0, E.transpose(), FF, ZZ_0);
-  MatrixXi delta_1 = mult(1, X_1.transpose(), D_1, E.transpose(), FF, ZZ_1);
+    */
 
-  // Truncation
-  // =========================== 
-  // =========================== 
+    MatrixXi F_0 = w_0 - V_0.col(j);
+    MatrixXi F_1 = w_1 - V_1.col(j);
+    MatrixXi F = rec(F_0, F_1);
 
-  // delta = X^T(X.w - Y)
-  w_0 = w_0 - (delta_0 / 128); // alpha/b = 2^-7 = 128; used in paper
-  w_1 = w_1 - (delta_1 / 128); // alpha/b = 2^-7 = 128; used in paper
+    // YY = X_B_j.w
+    MatrixXi YY_0 = mult(0, X_0.block(B * j,0,B,X.cols()), w_0, E.block(B * j,0,B,X.cols()), F, Z_0.col(j));
+    MatrixXi YY_1 = mult(1, X_1.block(B * j,0,B,X.cols()), w_1, E.block(B * j,0,B,X.cols()), F, Z_1.col(j));
 
-  // =========================== end for loop here ('t' epochs)
+    // D = X.w - Y
+    MatrixXi D_0 = YY_0 - Y_0.block(B * j,0,B,Y.cols());
+    MatrixXi D_1 = YY_1 - Y_1.block(B * j,0,B,Y.cols());
+
+    MatrixXi FF_0 = D_0 - VV_0.col(j);
+    MatrixXi FF_1 = D_1 - VV_1.col(j);
+    MatrixXi FF = rec(FF_0, FF_1);
+
+    // delta = X^T(X.w - Y)
+    MatrixXi delta_0 = mult(0, X_0.transpose().block(0,B * j,X.cols(),B), D_0, E.transpose().block(0,B * j,X.cols(),B), FF, ZZ_0.col(j));
+    MatrixXi delta_1 = mult(1, X_1.transpose().block(0,B * j,X.cols(),B), D_1, E.transpose().block(0,B * j,X.cols(),B), FF, ZZ_1.col(j));
+
+    // Truncation
+    // =========================== 
+    // =========================== 
+
+    // delta = X^T(X.w - Y)
+    w_0 = w_0 - (delta_0 / 128); // alpha/b = 2^-7 = 128; used in paper
+    w_1 = w_1 - (delta_1 / 128); // alpha/b = 2^-7 = 128; used in paper
+  }
+
   return rec(w_0, w_1);
 }
 
 MatrixXi idealLinearRegression(MatrixXi X, MatrixXi Y, MatrixXi w) // ideal functionality
 {
   // w -= a/|B| X^T (X.w - Y)
-  // =================================================================================
-  // following to be in a for loop, for 't' epochs:
-
-  MatrixXi YY = X * w; // YY = X.w
-  MatrixXi D = YY - Y; // D = X.w - Y
-  MatrixXi delta = X.transpose() * D; // delta = X^T(X.w - Y)
-  w = w - (delta / 128); // delta = X^T(X.w - Y)
-
-  // =========================== end for loop here ('t' epochs)
+  int t = (N * NUM_EPOCHS)/B; // E = 1
+  for(int i = 0; i < t; i ++)
+  {
+    MatrixXi YY = X.block(B * i,0,B,X.cols()) * w; // YY = X_B_i.w
+    MatrixXi D = YY - Y.block(B * i,0,B,Y.cols()); // D = X_B_i.w - Y_B_i
+    MatrixXi delta = X.transpose().block(0,B * i,X.cols(),B) * D; // delta = X^T_B_i(X.w - Y)
+    w = w - (delta / 128); // w -= a/B * delta
+  }
+  
   return w;
 }
 
 int main()
 {
-  MatrixXi X = MatrixXi::Random(3,5) / 10000000; // n = 3, d = 5, training samples
-  cout << "Here is the matrix X:\n" << X <<endl;
-  MatrixXi Y = MatrixXi::Random(3,1) / 10000000; // labels
-  //cout << "Here is the matrix Y:\n" << Y <<endl;
-  MatrixXi w = MatrixXi::Random(5,1) / 10000000;
+  MatrixXi X = MatrixXi::Random(N,d) / 10000000; // n = 6, d = 5, training samples
+  //cout << "Here is the matrix X:\n" << X <<endl;
+  //cout << "Here is the 1st col in X:\n" << X.col(1) <<endl;
+  //out << "Here is the matrix X_B_1:\n" << X.block(0 * B,0,B,X.cols()) <<endl;
+  //cout << "Here is the matrix X_B_2:\n" << X.block(1 * B,0,B,d) <<endl;
+  //cout << "Here is the matrix X_B_3:\n" << X.block(2 * B,0,B,d) <<endl;
+  //cout << "Here is the matrix X^T:\n" << X.transpose() <<endl;
+  //cout << "Here is the matrix X^T_B_1:\n" << X.transpose().block(0,0 * B,X.cols(),B) <<endl;
+  //cout << "Here is the matrix X^T_B_2:\n" << X.transpose().block(0,1 * B,X.cols(),B) <<endl;
+  //cout << "Here is the matrix X^T_B_3:\n" << X.transpose().block(0,2 * B,X.cols(),B) <<endl;
+
+
+  MatrixXi Y = MatrixXi::Random(N,1) / 10000000; // labels
+  cout << "Here is the matrix Y:\n" << Y <<endl;
+  MatrixXi w = MatrixXi::Random(d,1) / 10000000;
+
+  //int t = N * NUM_EPOCHS / B;
+
+  //MatrixXi U = MatrixXi::Random(N,d) / 100000000; 
+  //cout << "Here is the matrix U:\n" << U <<endl;
+  //MatrixXi V = MatrixXi::Random(d,t) / 100000000; 
+  //cout << "Here is the matrix V:\n" << V <<endl;
+  //MatrixXi VV = MatrixXi::Random(B,t) / 100000000;
+  //MatrixXi Z = MatrixXi::Zero(B,t); 
+  //MatrixXi ZZ = MatrixXi::Zero(d,t); 
+  
+  //for (int z = 0; z < Z.cols(); z++)
+  //{
+  //  Z.col(z) = U.block(z * B,0,B,X.cols()) * V.col(z);
+  //}
+
+  //for (int z = 0; z < ZZ.cols(); z++)
+  //{
+  //  ZZ.col(z) = U.transpose().block(0,z * B,X.cols(),B) * VV.col(z);
+  //}
+
+  //cout << "Here is the matrix Z:\n" << Z <<endl;
 
   MatrixXi new_w = linearRegression(X,Y,w);
-  cout << "Final weights are:\n" << new_w <<endl;
+  cout << "Final weights (under Privacy Preserving) are:\n" << new_w <<endl;
   MatrixXi ideal_w = idealLinearRegression(X,Y,w);
-  cout << "Final weights are:\n" << ideal_w <<endl;
+  cout << "Final weights (under Ideal Functionality) are:\n" << ideal_w <<endl;
 }
 
+void verify()
+{
   // Additive Secret Sharing
+
   /*
+
   MatrixXi shares[2];
 
   share(X, shares);
@@ -186,19 +250,19 @@ int main()
   share(Y, shares);
   MatrixXi Y_0 = shares[0];
   MatrixXi Y_1 = shares[1];
-  */
+
 
   //cout << "Here is the matrix X_0 + X_1:\n" << X_0 + X_1 <<endl;
   //cout << "Here is the matrix Y_0 + Y_1:\n" << Y_0 + Y_1 <<endl;
 
   // Multiplication
-  //MatrixXi w_0 = MatrixXi::Random(5,1) / 10000000;
-  //MatrixXi w_1 = MatrixXi::Random(5,1) / 10000000;
-  //MatrixXi w = rec(w_0, w_1);
+  MatrixXi w_0 = MatrixXi::Random(5,1) / 10000000;
+  MatrixXi w_1 = MatrixXi::Random(5,1) / 10000000;
+  MatrixXi w = rec(w_0, w_1);
   //cout << "Here is the matrix w_0 + w_1:\n" << w_0 + w_1 <<endl;
   
   //Masking
-  /*
+
   MatrixXi U = MatrixXi::Random(X.rows(),X.cols()) / 10000;
   share(U, shares);
   MatrixXi U_0 = shares[0]; 
@@ -232,9 +296,12 @@ int main()
   cout << "Here is the required product:\n" << prod_test <<endl;
   MatrixXi prod = rec(prod_0, prod_1);
   cout << "Here is the calculated product:\n" << prod <<endl;
+
   */
+}
+
+//MatrixXi pp = E*F + U*F + E*V + Z;
+//MatrixXi pp = -E*F + X*F + E*w + Z;
+//MatrixXi pp = E*F + X*F + E*w + Z; //--> doesn't work (SecureML)
 
 
-  //MatrixXi pp = E*F + U*F + E*V + Z;
-  //MatrixXi pp = -E*F + X*F + E*w + Z;
-  //MatrixXi pp = E*F + X*F + E*w + Z; //--> doesn't work (SecureML)
