0cca736c63a2528417ae5b20e8ac2b5cc81e795d
Kuber Shahi <kuber.shahi_ug21@ashoka.edu.in>
2021-06-18 13:58:31 +0530

    update test

--- test.cpp
+++ test.cpp
@@ -8,16 +8,20 @@
 using namespace std;
 using namespace Eigen;
 
-void insert(vector<int>  &l, int item){
-  l.push_back(item);
-}
+// void insert(vector<int>  &l, int item){
+//   l.push_back(item);
+// }
 
 int main(){
 
-  vector<int > l;
-  insert(l,1);
+  MatrixXf X = MatrixXf::Random(10,10);
   
-  for (int i = 0; i < 4; i++){
-    cout << l[i] << endl;
-  }
+  // for (int i = 0; i < 5; i++){
+  //   cout << X.row(i) << endl;
+  // }
+  cout << X << endl;
+  cout << "\n" << endl;
+  Map<MatrixXf> X1(X.data()+5,5,X.cols());
+
+  cout << X1 << endl;
 }
