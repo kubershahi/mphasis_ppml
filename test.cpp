12ef10dad37ccffa512c1f099bfa6ee99ae62b19
Kuber Shahi <kuber.shahi_ug21@ashoka.edu.in>
2021-06-20 00:53:27 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -12,16 +12,65 @@ using namespace Eigen;
 //   l.push_back(item);
 // }
 
+void bin(long n)
+  {
+    long i;
+    cout << "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        cout << "1";
+      }
+      else
+      {
+        cout << "0";
+      }
+    }
+  }
+
 int main(){
 
-  MatrixXf X = MatrixXf::Random(10,10);
-  
-  // for (int i = 0; i < 5; i++){
-  //   cout << X.row(i) << endl;
-  // }
-  cout << X << endl;
-  cout << "\n" << endl;
-  Map<MatrixXf> X1(X.data()+5,5,X.cols());
+  // MatrixXf X = MatrixXf::Random(1);
+  // MatrixXf Y = MatrixXf::Random(1);
 
-  cout << X1 << endl;
+  double X = 10.1578;
+  double Y = 5.2345;
+  double Z = X * Y; // the value we want ot approximate
+  
+
+  cout << endl << "X, 1010.001010: "<< X << endl;
+  cout << "Y, 101.001111: "<< Y << endl;
+  cout << "Z, 110101.001010111: " << Z << endl << endl;
+
+  X = X * 64; // precision of 6 binary decimal digits i.e 2^6
+  Y = Y * 64; // precision of 6 binary decimal digits i.e 2^6
+
+  cout << fixed;
+  cout << "After scaling by 6 bits i.e 2^6: " << endl;
+  cout << "X: "<< X << endl;
+  cout << "Y: "<< Y << endl << endl;
+  
+  cout << "After changing to integer " << endl;
+  int x = (int) X ;
+  int y = (int) Y ;
+
+  cout << "x, 1010001010: "<< x << endl;
+  cout << "y, 101001111 : "<< y << endl << endl;
+  
+  int z = x * y;
+  cout << "z before truncation, 110101001010010110: "<< z << endl;
+  z /= 64; // truncation product
+  cout << "z after  truncation, 110101001010      : "<< z << endl << endl;
+
+  double zz = (double) z;
+  zz /= 64;
+  cout << "z after cast,        110101.001010     : "<< zz << endl << endl;
+
+
+  // // selecting certain columns of a Matrix
+  // Map<MatrixXf> X1(X.data()+5,5,X.cols());
+
+  // casting a matrix;
+  // MatrixXi x = X.cast<int>();
 }
