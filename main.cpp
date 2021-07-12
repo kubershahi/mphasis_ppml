df4e5a048856826da4a12f6ed72a3d56cc78e3cd
actuallysoham <soham.de2001@gmail.com>
2021-07-12 08:41:26 +0530

    Dataset Selection Utility

--- main.cpp
+++ main.cpp
@@ -34,25 +34,13 @@ Dimensions:
 // Global Declarations: 
 // ==================================== 
 
-// for MNIST
-//int N = 1000; // 10000
-//int N_test = 100; // 1000
-//int d = 784; //784
-//int B = 128; //128
-//int NUM_EPOCHS = 5; 
-
 // for sanity check data
 int N = 6; //6
+int N_test = 6;
 int d = 2; //5
 int B = 3; //3
 int NUM_EPOCHS = 5;
 
-// for medical dataset
-//int N = 1070;
-//int N_test = 268;
-//int d = 5;
-//int B = 128;
-//int NUM_EPOCHS = 5;
 // ====================================
 
 int main(){
@@ -60,15 +48,31 @@ int main(){
   //==========================================
   // Loadin and Pre processing data:
   //==========================================
-/*
+
+  cout<<"Select Dataset (enter corresponding digit):"<<endl;
+  cout<<"\t [1] MNIST"<<endl;
+  cout<<"\t [2] Medical Insurance"<<endl;
+  cout<<"\t [3] Sanity Check"<<endl;
+  int selection = 0;
+  cout<<"Enter selection: ";
+  cin>>selection;
+
+  MatrixXd X,Y,w;
+
+  if (selection == 1){
+
+  //MNIST
+  :: N = 10000; // 10000
+  :: N_test = 1000; // 1000
+  :: d = 784; //784
+  :: B = 128; //128
+
   cout<<"Reading Data:"<<endl;
   // loading mnist dataset: training and testing portion separately
   vector<vector<double> > X_train;   // dim: 60000 x 784, 60000 training samples with 784 features
   vector<double> Y_train;            // dim: 60000 x 1  , the true label of each training sample
   
-  //read_insurance_data("datasets/medical/insurance_train.csv", X_train, Y_train); // for medical dataset
   read_data("datasets/mnist/mnist_train.csv", X_train, Y_train);             // for MNIST dataset
-  
 
   MatrixXd X1(N, d); // 60000, 784
   MatrixXd Y1(N, 1); // 60000, 1
@@ -79,13 +83,9 @@ int main(){
     Y1.row(i) = VectorXd::Map(&Y_train[i],1)/10.0;
   }
 
-  // cout << "Working: " << endl;
-
   vector<vector<double> > X_test;    // dim: 10000 x 784, 10000 testing samples with 784 features
   vector<double> Y_test;             // dim: 10000 x 1  , the true label of each testing sample
 
-
-  //read_insurance_data("datasets/medical/insurance_test.csv", X_test, Y_test); // for medical dataset
   read_data("datasets/mnist/mnist_test.csv", X_test, Y_test);                  // for MNIST dataset
 
   MatrixXd X1_test(N_test, d); // 1000, 784
@@ -98,10 +98,63 @@ int main(){
   }
 
   MatrixXd w1 = MatrixXd::Random(d,1);
-*/ 
-  //cout << "Here is Matrix w1:\n" << w1 <<endl;
-  //==========================================
 
+  X = X1;
+  Y = Y1;
+  w = w1;
+
+  }
+
+  else if (selection == 2){
+
+  //Medical Insurance
+  :: N = 1070; // 1070
+  :: N_test = 268; // 268
+  :: d = 5; // 5
+  :: B = 128; //128
+
+  cout<<"Reading Data:"<<endl;
+  // loading mnist dataset: training and testing portion separately
+  vector<vector<double> > X_train;   // dim: 60000 x 784, 60000 training samples with 784 features
+  vector<double> Y_train;            // dim: 60000 x 1  , the true label of each training sample
+  
+  read_insurance_data("datasets/medical/insurance_train.csv", X_train, Y_train); // for medical dataset  
+
+  MatrixXd X1(N, d); // 60000, 784
+  MatrixXd Y1(N, 1); // 60000, 1
+
+  for (int i = 0; i < N; i++)
+  {
+    X1.row(i) = VectorXd::Map(&X_train[i][0], d)/100.0;
+    Y1.row(i) = VectorXd::Map(&Y_train[i],1);
+  }
+
+  // cout << "Working: " << endl;
+
+  vector<vector<double> > X_test;    // dim: 10000 x 784, 10000 testing samples with 784 features
+  vector<double> Y_test;             // dim: 10000 x 1  , the true label of each testing sample
+
+
+  read_insurance_data("datasets/medical/insurance_test.csv", X_test, Y_test); // for medical dataset
+
+  MatrixXd X1_test(N_test, d); // 1000, 784
+  MatrixXd Y1_test(N_test, 1); // 1000, 1
+
+  for (int i = 0; i < N_test; i++)
+  {
+    X1_test.row(i) = VectorXd::Map(&X_test[i][0], d);
+    Y1_test.row(i) = VectorXd::Map(&Y_test[i],1);
+  }
+
+  MatrixXd w1 = MatrixXd::Random(d,1);
+
+  X = X1;
+  Y = Y1;
+  w = w1;
+
+  }
+
+  else {
   //==========================================
   // Sanity Check data:
   //==========================================
@@ -111,7 +164,12 @@ int main(){
   Y2 << 2,-14,1,-1,-7,-8;
   MatrixXd w2(2,1);
   w2 << 1.65921924,1.62628418;
-  //==========================================
+
+  X = X2;
+  Y = Y2;
+  w = w2;
+
+  }
 
 
   //==========================================
@@ -124,23 +182,25 @@ int main(){
   cout << "FLOATING LINEAR REGRESSION (SGD):"<<endl;
   cout << "=============================="<<endl<<endl;
 
-  MatrixXd ideal_w = idealLinearRegression(X2,Y2,w2);
-  //cout << "Final weights (under Ideal Functionality) are:\n" << ideal_w << endl << endl;
-  MatrixXd new_w = linearRegression(X2,Y2,w2);
+  MatrixXd ideal_w = idealLinearRegression(X,Y,w);
+  //cout << "Final weights (under Ideal Functionality) are:\n" << ideal_w << endl;
+  cout<<endl;
+  MatrixXd new_w = linearRegression(X,Y,w);
   //cout << "Final weights (under Privacy Preserving) are:\n" << new_w << endl << endl;
 
   cout << "=============================="<<endl;
   cout << "UINT-64 LINEAR REGRESSION (SGD):"<<endl;
   cout << "=============================="<<endl<<endl;
 
-  MatrixXi64 X_ = floattouint64(X2); // double to uint_64
-  MatrixXi64 Y_ = floattouint64(Y2); // double to uint_64
-  MatrixXi64 w_ = floattouint64(w2); // double to uint_64
+  MatrixXi64 X_ = floattouint64(X); // double to uint_64
+  MatrixXi64 Y_ = floattouint64(Y); // double to uint_64
+  MatrixXi64 w_ = floattouint64(w); // double to uint_64
 
   MatrixXi64 new_w_ = idealLinearRegression(X_,Y_,w_);
   MatrixXd new_w_f = uint64tofloat(new_w_); // descaling
 
-  //cout<<"Final weights (under Ideal Functionality)  are:\n "<< new_w_f << endl << endl;
+  //cout<<"Final weights (under Ideal Functionality)  are:\n "<< new_w_f << endl;
+  cout<<endl;
 
   new_w_ = linearRegression(X_,Y_,w_);
   new_w_f = uint64tofloat(new_w_); // descaling
